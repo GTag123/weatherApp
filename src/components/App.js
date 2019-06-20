79109e50fa305fae78892a8e186a7b2048feda4e
Tagir <gtagir11@yandex.ru>
2019-06-20 16:07:17 +0500

    React.js template

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/App.css';
+
+class App extends React.Component {
+    // render
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
